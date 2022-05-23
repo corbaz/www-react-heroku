a4baec450d1333a432cfb3b0d7a6ccb20aa022b7
Julio Corbaz <julio.corbaz@gmail.com>
2022-05-23 19:30:54 -0300

    Inicio main

---
+++ init.bat
@@ -0,0 +1,70 @@
+@echo off
+@REM set dir_raiz=%~dp0
+@REM Defino dir raiz y bat
+
+SET proyecto=www-react-heroku
+SET %github%_user=corbaz
+SET %heroku%_user=julio.corbaz@gmail.com
+
+set bat=bat
+set git=git
+set github=github
+set heroku=heroku
+
+set %github%_web=https://github.com/
+set %github%_profile=%github_web%%github_user%/
+set %github%_repo=%github_profile%%proyecto%.git
+
+set %heroku%_apps=https://dashboard.heroku.com/apps/
+set %heroku%_dashboard=%heroku_apps%%proyecto%
+set %heroku%_web=https://git.heroku.com/
+set %heroku%_repo=%herooku_webe%%proyecto%.git
+
+set dir_raiz=%cd%
+set dir_%bat%=%dir_raiz%\%bat%
+set dir_%bat%_%git%=%dir_raiz%\%bat%\%git%
+set dir_%bat%_%github%=%dir_raiz%\%bat%\%github%
+set dir_%bat%_%heroku%=%dir_raiz%\%bat%\%heroku%
+@REM agrego al PATH los directorios raiz y bat
+set PATH=%PATH%;%dir_raiz%;%dir_bat%;%dir_bat_git%;%dir_bat_github%;%dir_bat_heroku%
+
+SET pages_github=https://%github_user%.github.io/%proyecto%/
+SET pages_heroku=https://%proyecto%.herokuapp.com/
+
+SET rama_main=main
+SET rama_dev=dev
+SET rama_github=github
+SET rama_heroku=heroku
+
+@REM call npm create vite@latest %repo_name% -- --template react
+@REM /S	Copia directorios y subdirectorios, excepto los vacíos.
+@REM /E	Copia directorios y subdirectorios, incluidos los vacíos.
+git init 
+git add .
+git commit -m "Inicio main"
+
+git checkout -b %rama_dev%
+git checkout %rama_dev%
+call rd * /s /q
+
+git checkout main /www
+git checkout main /bat
+git branch -v
+
+pause
+
+
+
+
+
+
+@REM set /p URL=<list.txt
+@REM echo %URL%
+@REM more +1 list.txt | findstr /r /v "^$" > tmp_list.txt
+@REM set /p URL=<tmp_list.txt
+@REM echo %URL%
+
+@REM @REM Open the browser with that URL start "" "%URL%"
+@REM @REM start "" "%URL%"
+
+
